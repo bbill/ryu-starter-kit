450930e3f31b8342428e86cce4f06ea5730db349
bill <bill@t450.com>
2018-01-15 10:22:22 +0800

    addn  createdb in  install.sh.

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd
-[ -d ~/ryu/ryu/app ] || echo "Please install RYU first!"
+[ -d ~/ryu/ryu/app ] || echo "Please install RYU first!" && exit
 git clone https://github.com/bbill/ryu-starter-kit.git ~/ryu/ryu/app/tooyum
 sudo apt-get install -y mysql-server libmysqld-dev
 sudo pip install networkx MySQL-python
+~/ryu/ryu/app/tooyum/createdb.sh
+
 
